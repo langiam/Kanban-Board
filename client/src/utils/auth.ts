8c0054bf86a39b80d930f3f2f2ca905d133b5179
langiam <ryan.matthew.lang@gmail.com>
2025-05-20 17:39:27 -0400

    updated client auth file

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,51 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
 
 class AuthService {
-  getProfile() {
+  getProfile(): JwtPayload | null {
     // TODO: return the decoded token
+    const token = this.getToken();
+    if (!token) return null;
+    try {
+      return jwtDecode<JwtPayload>(token);
+    } catch {
+      return null;
+    }
   }
 
-  loggedIn() {
+  loggedIn(): boolean {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
-  isTokenExpired(token: string) {
+  isTokenExpired(token: string): boolean {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const { exp } = jwtDecode<JwtPayload>(token);
+      if (!exp) return false;
+      return Date.now() >= exp * 1000;
+    } catch {
+      return true;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    return localStorage.getItem('kanban_token') || '';
   }
 
-  login(idToken: string) {
+  login(idToken: string): void {
     // TODO: set the token to localStorage
+    localStorage.setItem('kanban_token', idToken);
     // TODO: redirect to the home page
+    window.location.href = '/';
   }
 
-  logout() {
+  logout(): void {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('kanban_token');
     // TODO: redirect to the login page
+    window.location.href = '/login';
   }
 }
 
