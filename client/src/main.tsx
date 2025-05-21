04d56f82abc29fab76e857ef62827b4d641fcca4
langiam <ryan.matthew.lang@gmail.com>
2025-05-21 11:47:31 -0400

    adding finished client tweaks for deployment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,41 +1,50 @@
+// client/src/main.tsx
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
 import './index.css';
 
-import App from './App.tsx';
-import Board from './pages/Board.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-import EditTicket from './pages/EditTicket.tsx';
+import App         from './App.tsx';
+import Board       from './pages/Board.tsx';
+import EditTicket  from './pages/EditTicket.tsx';
 import CreateTicket from './pages/CreateTicket.tsx';
-import Login from './pages/Login.tsx';
+import Login       from './pages/Login.tsx';
+import ErrorPage   from './pages/ErrorPage.tsx';
+import PrivateRoute from './components/PrivateRoute.tsx';
 
 const router = createBrowserRouter([
+  // Public login route
+  {
+    path: '/login',
+    element: <Login />,
+  },
+
+  // Protected routes
   {
     path: '/',
-    element: <App />,
+    element: (
+      <PrivateRoute>
+        <App />
+      </PrivateRoute>
+    ),
     errorElement: <ErrorPage />,
     children: [
-      {
-        index: true,
-        element: <Board />
-      }, 
-      {
-        path: '/edit',
-        element: <EditTicket />
-      },
-      {
-        path: '/create',
-        element: <CreateTicket />
-      },
-      {
-        path: '/login',
-        element: <Login />
-      }
-    ]
-  }
-])
+      { index: true,      element: <Board /> },
+      { path: 'create',   element: <CreateTicket /> },
+      { path: 'edit/:id', element: <EditTicket /> },
+    ],
+  },
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
-}
+  // Catch-all redirect to login
+  {
+    path: '*',
+    element: <Navigate to="/login" replace />,
+  },
+]);
+
+ReactDOM.createRoot(
+  document.getElementById('root')!
+).render(<RouterProvider router={router} />);
